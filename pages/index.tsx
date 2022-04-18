fa5e9f20ed066434e8e0a8da26f2a34247cb2e1a
BangHyeonJun <davidbang@cj.net>
2022-04-19 00:49:10 +0900

    앱 체크 로직 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -81,10 +81,8 @@ function Home({ cats }: HomeProps) {
 	useEffect(() => {
 		let isApp = false;
 
+		console.log();
 		if (window.matchMedia("(display-mode: standalone)").matches) {
-			// do things here
-			// set a variable to be used when calling something
-			// e.g. call Google Analytics to track standalone use
 			isApp = true;
 		}
 
